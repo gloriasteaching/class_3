29d0d4a0dab53ef70635e4ba5f7432a8583ef031
gloriasteaching <47659579+gloriasteaching@users.noreply.github.com>
2022-12-30 12:45:23 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -36,4 +36,25 @@ let MySprite2 = sprites.create(img`
     7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
     `, SpriteKind.Enemy)
 controller.moveSprite(mySprite)
-MySprite2.follow(mySprite)
+MySprite2.follow(mySprite, 15)
+MySprite2.setPosition(100, 0)
+let MySprite3 = sprites.create(img`
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a 1 1 1 1 1 a a 1 1 1 1 1 a a 
+    a a 1 1 1 1 1 a a 1 1 1 1 1 a a 
+    a a f f f 1 1 a a f f f 1 1 a a 
+    a a f f f 1 1 a a f f f 1 1 a a 
+    a a f f f 1 1 a a f f f 1 1 a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a 4 4 4 a a a a a a a a 
+    a a a a a 4 4 4 a a a a a a a a 
+    a a a a a 4 4 4 a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    `, SpriteKind.Player)
+MySprite3.setPosition(4, 6)
+MySprite3.follow(MySprite2, 10)
